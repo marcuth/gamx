e402db11e0581f27a54cf51b78c4d5218e60d859
Marcuth <thiago.playgames15@gmail.com>
2024-02-23 11:08:24 -0300

    Adicionando o conceito de props ao Component

--- src/ui/component.ts
+++ src/ui/component.ts
@@ -1,4 +1,14 @@
-abstract class Component {
+abstract class Component<Props = void> {
+    private props: Props
+
+    public constructor(props: Props) {
+        this.props = props as Props
+    }
+
+    public setProps(newProps: Props): void {
+        this.props = newProps
+    }
+
     public abstract draw(ctx: CanvasRenderingContext2D): any
 }
 
